3038a09e5b4d838049b393a4924565e0304d9020
paliszoman <kamilkrolik@hotmail.com>
2022-12-10 20:28:00 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,16 @@ import { Filter } from './Filter/Filter.jsx';
 import { ContactList } from './ContactList/ContactList.jsx';
 
 export const App = () => {
-  let contactsArray = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-
   let [contacts, setContacts] = useState([]);
   let [filterMe, setFilter] = useState('');
 
   useEffect(() => {
+    let contactsArray = [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ];
     if (contacts.length <= 0) {
       setContacts(
         (contacts =
@@ -28,7 +27,7 @@ export const App = () => {
     if (contacts !== localStorage.getItem('phonebook')) {
       localStorage.setItem('phonebook', JSON.stringify(contacts));
     }
-  });
+  }, [contacts]);
 
   //function for searching contacts by single letter
   const searchForContacts = searchFromFilter => {
