a64434c6918b45185a1b6dd4df666bb2b1d0bb49
paliszoman <kamilkrolik@hotmail.com>
2022-12-10 20:47:43 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,19 @@ import { Filter } from './Filter/Filter.jsx';
 import { ContactList } from './ContactList/ContactList.jsx';
 
 export const App = () => {
+  /* let contactsArray = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];*/
   let [contacts, setContacts] = useState([]);
   let [filterMe, setFilter] = useState('');
 
   useEffect(() => {
-    let contactsArray = [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ];
+    let contacts = JSON.parse(localStorage.getItem('phonebook'));
     if (contacts.length <= 0) {
-      setContacts(
-        (contacts =
-          JSON.parse(localStorage.getItem('phonebook')) || contactsArray)
-      );
+      setContacts(contacts);
     }
   }, []);
 
